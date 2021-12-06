f6f48f1509a2382f9ad886e401a8477c5b205cd3
optikool <optikool@yahoo.com>
2021-12-06 00:36:09 -0800

    Added tests for Resolvers

--- src/app/resolvers/character.resolver.spec.ts
+++ src/app/resolvers/character.resolver.spec.ts
@@ -1,16 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, waitForAsync } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, convertToParamMap, RouterStateSnapshot } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { myCharacters } from '../services/character.service';
+import { initialState } from '../views/characters/store';
 
 import { CharacterResolver } from './character.resolver';
 
 describe('CharacterResolver', () => {
-  let resolver: CharacterResolver;
+  let characterResolver: CharacterResolver;
+  let store: MockStore;
+  let state: RouterStateSnapshot;
+  let route = {
+    paramMap: {
+      get: (id) => 2
+    }
+  } as unknown as ActivatedRouteSnapshot;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    resolver = TestBed.inject(CharacterResolver);
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
+      providers: [
+        provideMockStore({ initialState }),
+        { provide: ActivatedRouteSnapshot, useValue: { snapshot: { paramMap: convertToParamMap({ id: 2 })}}},
+        { provide: RouterStateSnapshot, useValue: { snapshot: { url: '' }}},
+      ]
+    })
+    .compileComponents()
+    .then(() => {
+      characterResolver = TestBed.inject(CharacterResolver);
+      state = TestBed.inject(RouterStateSnapshot);
+      store = TestBed.inject(MockStore);
+      store.setState({
+        characters: myCharacters
+      });
+      store.refreshState();
+    });
+  }));
 
   it('should be created', () => {
-    expect(resolver).toBeTruthy();
+    expect(characterResolver).toBeTruthy();
   });
+
+  it('should call store dispach', () => {
+    spyOn(store, 'dispatch');
+    const spyRoute = spyOn(route.paramMap, 'get');
+    spyRoute.and.returnValue('2');
+    characterResolver.resolve(route, state);
+    expect(store.dispatch).toHaveBeenCalledTimes(2);
+  });
+
+  it('should return NONE Observable', fakeAsync(() => {
+    characterResolver.resolve(route, state).subscribe(result => {
+      expect(result).toEqual('NONE');
+    });
+  }));
 });
--- src/app/resolvers/characters.resolver.spec.ts
+++ src/app/resolvers/characters.resolver.spec.ts
@@ -1,16 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, convertToParamMap, RouterStateSnapshot } from '@angular/router';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { myCharacters } from '../services/character.service';
+import { initialState } from '../views/characters/store';
 
 import { CharactersResolver } from './characters.resolver';
 
 describe('CharactersResolver', () => {
-  let resolver: CharactersResolver;
+  let charactersResolver: CharactersResolver;
+  let store: MockStore;
+  let route: ActivatedRouteSnapshot;
+  let state: RouterStateSnapshot;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    resolver = TestBed.inject(CharactersResolver);
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({ initialState }),
+        { provide: ActivatedRouteSnapshot, useValue: { snapshot: { paramMap: convertToParamMap({ id: 1 }) }}},
+        { provide: RouterStateSnapshot, useValue: { snapshot: { url: '' }}},
+      ]
+    })
+    .compileComponents()
+    .then(() => {
+      charactersResolver = TestBed.inject(CharactersResolver);
+      route = TestBed.inject(ActivatedRouteSnapshot);
+      state = TestBed.inject(RouterStateSnapshot);
+      store = TestBed.inject(MockStore);
+      store.setState({
+        characters: myCharacters
+      });
+      store.refreshState();
+    });
+  }));
 
   it('should be created', () => {
-    expect(resolver).toBeTruthy();
+    expect(charactersResolver).toBeTruthy();
+  });
+
+  it('should call store dispach', () => {
+    spyOn(store, 'dispatch');
+    charactersResolver.resolve(route, state);
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return NONE Observable', () => {
+    charactersResolver.resolve(route, state).subscribe(result => {
+      expect(result).toEqual('NONE');
+    });
   });
 });
