4b9dffa429ff35460c162f8531c1e42689d95ec1
optikool <optikool@yahoo.com>
2021-12-07 00:31:23 -0800

    Added tests for character service

--- src/app/services/character.service.spec.ts
+++ src/app/services/character.service.spec.ts
@@ -4,63 +4,16 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { CharacterService, myCharacters } from './character.service';
 import { routes } from '../app-routing.module';
 import { ElectronService } from 'ngx-electron';
-import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
-import { ipcRenderer } from 'electron';
-import { FakeElectronService } from './fake-electron.service';
 
-// class Channel {
-//   constructor(public name: string, public listener: Function) {}
-// }
-
-// export class Message {
-//   channel: string;
-//   params?: any[];
-// }
-
-// class FakeElectronService {
-  // channelSource = new Subject<Message>();
-
-  // private channels: Channel[] = [];
-
-  // ipcRenderer = {
-  //   // on: (name: string, listener: Function) => {
-  //   //   this.channels.push(new Channel(name, listener));
-  //   // },
-  //   // once: (name: string, listener: Function) => {
-  //   //   this.channels.push(new Channel(name, listener));
-  //   // },
-  //   sendSync: jasmine.createSpy().and.stub()
-  //   // sendSync: (channel: string, args: string[]) => {
-  //   //   return {
-  //   //     response: 'okay'
-  //   //   }
-  //   // }
-  // };
-
-  // ipcRenderer() {
-  //   return jasmine.createSpy().and.returnValue({sendSync: jasmine.createSpy()})
-
-    // return function sendSync(channel: string, args: string[]) {
-
-    // }
-  // }
-
-  // constructor() {
-    // this.channelSource.subscribe(msg => {
-    //   this.channels.find(channel => channel.name === msg.channel).listener({}, ...msg.params);
-    // });
-  // }
-// }
-
-const fakeElectronService = {
-  ipcRenderer: jasmine.createSpy().and.returnValue({sendSync: jasmine.createSpy().and.returnValue('get-characters')})
-}
-
-xdescribe('CharacterService', () => {
+describe('CharacterService', () => {
   let service: CharacterService;
-  let routerSpy: Router;
-  let electronService: ElectronService;
+  let routerSpy: Router = jasmine.createSpyObj(Router, ['navigate']);
+  const electronService = {
+    ipcRenderer: {
+      sendSync: () => myCharacters
+    }
+  } as unknown as ElectronService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -68,13 +21,11 @@ xdescribe('CharacterService', () => {
       providers: [
         CharacterService,
         { provide: Router, useValue: routerSpy },
-        { provide: ElectronService, useValue: FakeElectronService }
+        { provide: ElectronService, useValue: electronService }
       ],
     }).compileComponents()
     .then(() => {
-      routerSpy = jasmine.createSpyObj(Router, ['navigate']);
       service = TestBed.inject(CharacterService);
-      electronService = TestBed.inject(ElectronService);
     });    
   }));
 
@@ -88,25 +39,24 @@ xdescribe('CharacterService', () => {
         expect(result.characterName).toBe('Seth');
         expect(result.gender).toBe('M');
         expect(result.id).toBe(3);
-        
       });
   });
 
-  xit('should get characters', () => {
-    spyOn(electronService.ipcRenderer, 'sendSync').and.stub();
-
+  it('should get characters', () => {
     service.getCharacters()
       .subscribe(result => {
-        console.log('result: ', result);
-        expect(result.length).toEqual(5);
+        expect(result.length).toEqual(8);
         expect(electronService.ipcRenderer.sendSync).toHaveBeenCalled();
+        expect(electronService.ipcRenderer).toHaveBeenCalledTimes(1);
       });
   });
 
   it('should call navigateToPage', () => {
     const nav = ['/character-edit', '2'];
     service.navigateToPage(nav);
+    expect(routerSpy.navigate).toHaveBeenCalled();
     expect(routerSpy.navigate).toHaveBeenCalledWith(nav);
+    expect(routerSpy.navigate).toHaveBeenCalledTimes(1);
   });
 
   it('should createCharacter', () => {
